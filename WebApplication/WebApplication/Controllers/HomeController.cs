21f919452b4e97d0a5f82916dcfcf4cbd479341d
Richard Wysocki <richardwysocki@gmail.com>
2016-01-23 13:25:11 -0500

    Make change

--- WebApplication/WebApplication/Controllers/HomeController.cs
+++ WebApplication/WebApplication/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace WebApplication.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your RRW application description page.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your RRW contact page.";
 
             return View();
         }
