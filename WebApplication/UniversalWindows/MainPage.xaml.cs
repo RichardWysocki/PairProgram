8cac504fc6d659a1ef5c2faafce1c606c981997d
Richard Wysocki <richardwysocki@gmail.com>
2016-04-12 23:23:29 -0400

    Update Files with Generic Validation for Person Insert

--- WebApplication/UniversalWindows/MainPage.xaml.cs
+++ WebApplication/UniversalWindows/MainPage.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Microsoft.Advertising.WinRT.UI;
 using UniversalWindows.Common;
 using PersonModel = UniversalWindows.Model.PersonModel;
+using UniversalWindows.Model;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -23,19 +25,24 @@ namespace UniversalWindows
               
         private async void button_Click(object sender, RoutedEventArgs e)
         {
-            var loadExistingData = await loadData();
-            var peopleList = new List<PersonModel>();
-            var storageHelper = new StorageHelper<List<PersonModel>>(StorageType.Local);
             var person = new PersonModel(Name.Text, Email.Text, Phone.Text);
-            if (loadExistingData != null)
-            {                
-                loadExistingData.Add(person);
-                storageHelper.SaveASync(loadExistingData, "Settings");
-            }
-            else
-            {
-                peopleList.Add(person);
-                storageHelper.SaveASync(peopleList, "Settings");
+
+            if (ValidatePerson(person))
+            { 
+                var loadExistingData = await loadData();
+                var peopleList = new List<PersonModel>();
+                var storageHelper = new StorageHelper<List<PersonModel>>(StorageType.Local);
+                
+                if (loadExistingData != null)
+                {                
+                    loadExistingData.Add(person);
+                    storageHelper.SaveASync(loadExistingData, "Settings");
+                }
+                else
+                {
+                    peopleList.Add(person);
+                    storageHelper.SaveASync(peopleList, "Settings");
+                }
             }
         }
 
@@ -67,9 +74,35 @@ namespace UniversalWindows
             return peopleList;
         }
 
-        private void button_Copy_Click(object sender, RoutedEventArgs e)
+        private void NavToManagement_Click(object sender, RoutedEventArgs e)
         {
-            Frame.Navigate(typeof(BlankPage1));
+            Frame.Navigate(typeof(ManagementPage));
         }
+
+        private bool ValidatePerson(PersonModel person)
+        {
+            return ValidateAllFieldsComplete(person) & ValidateEmail(person.Email);
+        }
+
+        private bool ValidateAllFieldsComplete(PersonModel person)
+        {
+            return person.Email.Length > 0 & person.Name.Length > 0 & person.Phone.Length > 0;
+        }
+
+        private bool ValidateEmail(string personEmail)
+        {
+            string email = personEmail;
+            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            Match match = regex.Match(email);
+            if (match.Success)
+                return true;
+            else
+                return false;
+            //if (match.Success)
+            //    lbl_message.Text = email + " is Valid Email Address";
+            //else
+            //    lbl_message.Text = email + " is Invalid Email Address";
+        }
+
     }
 }
--- WebApplication/UniversalWindows/ManagementPage.xaml.cs
+++ WebApplication/UniversalWindows/ManagementPage.xaml.cs
@@ -13,9 +13,9 @@ namespace UniversalWindows
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class BlankPage1 : Page
+    public sealed partial class ManagementPage : Page
     {
-        public BlankPage1()
+        public ManagementPage()
         {
             InitializeComponent();
         }
