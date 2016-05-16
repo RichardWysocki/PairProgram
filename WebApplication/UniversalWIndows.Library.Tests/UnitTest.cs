613ac80b176aae0e9af18b0655772514c2118f49
Richard Wysocki <richardwysocki@gmail.com>
2016-05-16 17:23:40 -0400

    Add New Unit Test Project for UWP

---
+++ WebApplication/UniversalWIndows.Library.Tests/UnitTest.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace UniversalWIndows.Library.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            var x = 1;
+            Assert.AreEqual(1,x,"are equal");
+        }
+    }
+}
